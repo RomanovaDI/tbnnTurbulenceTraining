eb1b4771622cde86f9d685ac6770ebe453d206c8
Daria Romanova <romanovadi@gmail.com>
2022-11-14 00:08:44 +0300

    Preparing changed because of mapFields utilite
    Signed-off-by: Daria Romanova <romanovadi@gmail.com>

--- constantAngleSlopeDNS_U4e-1_A5_H1e-2_RANSsets/prepareCases.py
+++ constantAngleSlopeDNS_U4e-1_A5_H1e-2_RANSsets/prepareCases.py
@@ -10,35 +10,19 @@ def timeStepsList(DNSfolder):
 	timeStepsList = timeStepsList[index]
 	return timeStepsList[1:]
 
-def prepareFolders(DNSfolder, RANSfolder, TSL, deltaT):
+def prepareFolders(DNSfolder, RANSfolder, TSL):
 	sp.call('rm -rf '+RANSfolder+'[0-9]*', shell=True)
-	for time in TSL:
-		sp.run('cp -r '+RANSfolder+'orig '+RANSfolder+time+';\
-				cd '+RANSfolder+time+';\
+	for n in range(len(TSL)-1):
+		sp.run('cp -r '+RANSfolder+'origBM '+RANSfolder+TSL[n]+';\
+				cd '+RANSfolder+TSL[n]+';\
 				pwd;\
-				cp ../'+DNSfolder+time+'/USpAv 0/U;\
-				cp ../'+DNSfolder+time+'/awSpAv 0/alpha.water;\
-				cp ../'+DNSfolder+time+'/p_rghSpAv 0/p_rgh;\
-				mv 0 '+time, shell=True, check=True)
-		with fi.FileInput(RANSfolder+time+'/system/controlDict', inplace=True) as file:
-			for line in file:
-				print(line.replace('startTimePattern', time), end='')
-		with fi.FileInput(RANSfolder+time+'/system/controlDict', inplace=True) as file:
-			for line in file:
-				print(line.replace('endTimePattern', str(float(time)+deltaT)), end='')
-		with fi.FileInput(RANSfolder+time+'/system/controlDict', inplace=True) as file:
-			for line in file:
-				print(line.replace('deltaTPattern', str(deltaT)), end='')
-		sp.run('cd '+RANSfolder+time+';\
-				./Allrun', shell=True, check=True)
-	for n in range(len(TSL)-1):
-		sp.run('cp '+DNSfolder+TSL[n+1]+'/USpAv '+RANSfolder+TSL[n]+'/'+TSL[n+1]+'/Uref', shell=True, check=True)
-	for n in range(len(TSL)-1):
-		sp.run('cp '+DNSfolder+TSL[n+1]+'/awSpAv '+RANSfolder+TSL[n]+'/'+TSL[n+1]+'/AWref', shell=True, check=True)
-	for n in range(len(TSL)-1):
-		sp.run('cp '+DNSfolder+TSL[n+1]+'/p_rghSpAv '+RANSfolder+TSL[n]+'/'+TSL[n+1]+'/p_rghref', shell=True, check=True)
-	for n in range(len(TSL)-1):
-		sp.run('cp -r '+RANSfolder+TSL[n]+'/'+TSL[n+1]+' '+RANSfolder+TSL[n]+'/'+TSL[n]+'/res', shell=True, check=True)
+				mapFields -consistent -sourceTime \''+TSL[n][:-1]+'\' ../../constantAngleSlopeDNS_U4e-1_A5_H1e-2_NotDNS/;\
+				./Allrun;\
+				mv 0 init;\
+				mv 1e-05 res;\
+				mapFields -consistent -sourceTime \''+TSL[n+1][:-1]+'\' ../../constantAngleSlopeDNS_U4e-1_A5_H1e-2_NotDNS/;\
+				mv 0 ref',
+				shell=True, check=True)
 
 def main():
 	DNSfolder = '../constantAngleSlopeDNS_U4e-1_A5_H1e-2_NotDNS/'
@@ -47,9 +31,9 @@ def main():
 	MLturbRANSfolder = 'TIF'
 	KEturbRANSfolder = 'KEIF'
 	KWturbRANSfolder = 'KWIF'
-	prepareFolders(DNSfolder, MLturbRANSfolder, TSL, deltaT)
-	prepareFolders(DNSfolder, KEturbRANSfolder, TSL, deltaT)
-	prepareFolders(DNSfolder, KWturbRANSfolder, TSL, deltaT)
+	prepareFolders(DNSfolder, MLturbRANSfolder, TSL)
+	prepareFolders(DNSfolder, KEturbRANSfolder, TSL)
+	prepareFolders(DNSfolder, KWturbRANSfolder, TSL)
 
 if __name__ == "__main__":
-    main()
+	main()
